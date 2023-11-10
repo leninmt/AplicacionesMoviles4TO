c2d840aafdcb5ba47f9229caa9d8b4d06bfdf99b
unknown <bsm.latacumba@yavirac.edu.ec>
2023-11-10 08:48:40 -0500

    primer commit

--- build/windows/flutter/cmake_install.cmake
+++ build/windows/flutter/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/cuarto_a/navegacion/navegar/windows/flutter
+# Install script for directory: C:/cuarto_a_n/navegacion/NavLM/navegacion/windows/flutter
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
