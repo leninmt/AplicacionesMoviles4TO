e032b320d15d6589f628295dc3841f6ade40398a
leninmt <ljc.montalvo@yavirac.edu.ec>
2023-11-13 08:39:48 -0500

    taller clase

--- lib/src/widgets/input_text.dart
+++ lib/src/widgets/input_text.dart
@@ -10,23 +10,39 @@ class InputText extends StatelessWidget {
   final bool obsecure;
   final void Function(String data) onChanged;
   final String Function(String data) validator;
-  const InputText(Key? key
-    this.label,
-    this.hint,
-    this.icono,
-    this.keyboard,
-    this.obsecure,
-    this.onChanged,
-    this.validator,
-
-  )
-
-
-
-  const InputText({super.key});
+  const InputText({
+    key,
+    this.label="",
+    this.hint="",
+    required this.icono,
+    this.keyboard=TextInputType.text,
+    required this.obsecure= false,
+    required this.onChanged,
+    required this.validator,
+  }
+  ): super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: TextFormField(
+        keyboardType: this.keyboard,
+        obscureText:this.obsecure,
+        onChanged: this.onChanged,
+        //validator: this.validator,
+        decoration: InputDecoration(
+          hintText: this.hint,
+          labelText: this.label,
+          labelStyle: TextStyle(
+            color: Colors.blueGrey,
+            fontFamily: "FredokaOne",
+            fontSize: 25.0, 
+          ),
+          suffixIcon: this.icono,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular( 20.0) )
+
+        ),
+      ),
+    );
   }
 }
--- lib/src/widgets/login_form.dart
+++ lib/src/widgets/login_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navegar/src/widgets/input_text.dart';
 
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
@@ -13,8 +14,15 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Hola"),
+    return Form(
+      child: Column(
+        children:<Widget> [
+          InputText(hint: "Email Adrees",
+          label: "Email Adrees",
+          keyboard: TextInputType.emailAddress, icono:  ,
+          )
+        ],
+      ),
     );
   }
 }
